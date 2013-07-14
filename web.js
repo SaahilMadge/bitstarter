1557c25769ac0b48c2c495a273b680a1a0fdbbb6
SaahilMadge <smadge@princeton.edu>
2013-07-14 17:19:39 +0000

    web.js

--- web.js
+++ web.js
@@ -2,12 +2,12 @@ var fs = require('fs')
 
 var express = require('express');
 
-var app = express.createServer(express.logger());
+var app = express();
 
 var textBuffer = new Buffer(fs.readFileSync("index.html"), 'uftf-8');
 
 app.get('/', function(request, response) {
-  response.send(textBuffer.toString();
+  response.send(textBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
